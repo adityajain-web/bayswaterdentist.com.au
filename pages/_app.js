9fd3f8914b731cb5f1e7e9cab3c036b39ce85c9f
Pritam Shinde <44087724+pritam-shinde@users.noreply.github.com>
2022-08-05 12:29:24 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -41,6 +41,23 @@ function MyApp({ Component, pageProps }) {
     })(window,document,'script','dataLayer','GTM-TP2SRNH');`}
     </Script>
     {/* <!-- End Google Tag Manager --> */}
+    {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+    <Script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=UA-212912537-1"
+      strategy="afterInteractive"
+    ></Script>
+    <Script id="google-analytics" strategy="afterInteractive">
+      {
+        `window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+  
+        gtag("config", "UA-212912537-1");`
+      }
+    </Script>
 
     {/* <!-- Google Tag Manager (noscript) --> */}
     <noscript dangerouslySetInnerHTML={{
@@ -48,9 +65,9 @@ function MyApp({ Component, pageProps }) {
       height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>`}}></noscript>
     {/* <!-- End Google Tag Manager (noscript) --> */}
 
-    <Script strategy='lazyOnload' data-he-id="89445" data-alignment="left" data-he-fixed="true" data-background-color="#003a4c" src="https://healthengine.com.au/webplugin/appointments.js"></Script>
+    <Script strategy="afterInteractive" data-he-id="89445" data-alignment="left" data-he-fixed="true" data-background-color="#003a4c" src="https://healthengine.com.au/webplugin/appointments.js"></Script>
 
-     <Script strategy='lazyOnload' defer src="https://connect.podium.com/widget.js#ORG_TOKEN=6fcbac5a-ab67-46ba-adae-2b930504d726" id="podium-widget" data-organization-api-token="6fcbac5a-ab67-46ba-adae-2b930504d726"></Script> 
+     <Script strategy="afterInteractive" defer src="https://connect.podium.com/widget.js#ORG_TOKEN=6fcbac5a-ab67-46ba-adae-2b930504d726" id="podium-widget" data-organization-api-token="6fcbac5a-ab67-46ba-adae-2b930504d726"></Script> 
     <Header />
     <Component {...pageProps} />
     <Footer />
