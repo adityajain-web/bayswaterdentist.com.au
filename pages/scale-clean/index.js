fa86662e92e150fcd69615e3d3c3bdf1bd90dce0
Pritam Shinde <pritamshinde.success@gmail.com>
2022-09-13 14:39:29 +0530

    last update 130920221439

--- pages/scale-clean/index.js
+++ pages/scale-clean/index.js
@@ -38,7 +38,7 @@ const ScaleClean = () => {
   const Popup = () => {
     return (<>
         <Container maxWidth="xxl" style={{ height: "100%", backgroundColor: "rgba(0,0,0,0.7)", position: "absolute", top: 0, left: 0, right: 0, bottom: 0, zIndex: 1 }} className="d-flex justify-content-center align-items-center">
-            <Box p={3}>
+            <Box mt={10} p={3}>
                 <Box className='d-flex justify-content-end'>
                     <IconButton onClick={() => setOpen(true)}>
                         <Close style={{ color: "#fff" }} />
